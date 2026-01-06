4aa2ac3987479e2b9cdc7c99f988a3c0ce935801
Tareeqee <tareeqee@gmail.com>
2026-01-06 07:42:24 +0500

    more image uploaded

--- data/index.ts
+++ data/index.ts
@@ -43,8 +43,8 @@ export const gridItems = [
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
     titleClassName: "justify-start",
-    img: "/grid.svg",
-    spareImg: "/b4.svg",
+    img: "/portfolio/grid.svg",
+    spareImg: "/portfolio/b4.svg",
   },
 
   {
@@ -54,8 +54,8 @@ export const gridItems = [
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
-    spareImg: "/grid.svg",
+    img: "/portfolio/b5.svg",
+    spareImg: "/portfolio/grid.svg",
   },
   {
     id: 6,
@@ -74,16 +74,16 @@ export const projects = [
     id: 1,
     title: "Post Covid Market Analysis",
     des: "As a Business Analyst & Data Analyst at ZogGlobal, I conducted market research and user behavior analysis to guide the development market. I translated insights into actionable recommendations, improving user engagement and aligning simulation features with business goals.",
-    img: "/p1.jpg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    img: "/portfolio/p1.jpg",
+    iconLists: ["/portfolio/re.svg", "/portfolio/tail.svg", "/portfolio/ts.svg", "/portfolio/three.svg", "/portfolio/fm.svg"],
     link: "https://zogglobal.com",
   },
   {
     id: 2,
     title: "Accuracy In Analytics",
     des: " As a Business Analyst to improve accuracy in decision making I analyzed usage patterns, defined KPIs, created data dashboards, and provided actionable insights that enhanced user experience and drove feature adoption.",
-    img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    img: "/portfolio/p2.svg",
+    iconLists: ["/portfolio/next.svg", "/portfolio/tail.svg", "/portfolio/ts.svg", "/portfolio/stream.svg", "/portfolio/c.svg"],
     link: "https://cyberiasoftwaresolution.com",
   },
 ];
@@ -125,32 +125,32 @@ export const companies = [
   {
     id: 1,
     name: "cloudinary",
-    img: "/cloud.svg",
-    nameImg: "/cloudName.svg",
+    img: "/portfolio/cloud.svg",
+    nameImg: "/portfolio/cloudName.svg",
   },
   {
     id: 2,
     name: "appwrite",
-    img: "/app.svg",
-    nameImg: "/appName.svg",
+    img: "/portfolio/app.svg",
+    nameImg: "/portfolio/appName.svg",
   },
   {
     id: 3,
     name: "HOSTINGER",
-    img: "/host.svg",
-    nameImg: "/hostName.svg",
+    img: "/portfolio/host.svg",
+    nameImg: "/portfolio/hostName.svg",
   },
   {
     id: 4,
     name: "stream",
-    img: "/s.svg",
-    nameImg: "/streamName.svg",
+    img: "/portfolio/s.svg",
+    nameImg: "/portfolio/streamName.svg",
   },
   {
     id: 5,
     name: "docker.",
-    img: "/dock.svg",
-    nameImg: "/dockerName.svg",
+    img: "/portfolio/dock.svg",
+    nameImg: "/portfolio/dockerName.svg",
   },
 ];
 
@@ -160,45 +160,45 @@ export const workExperience = [
     title: "Business Analyst ",
     desc: "Conducted requirements gathering, competitor research, and delivered data-driven insights to support strategic decisions in a 3D simulation platform project.",
     className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
+    thumbnail: "/portfolio/exp1.svg",
   },
   {
    id: 2,
     title: "Frontend Developer ",
     desc: "Built and optimized responsive web interfaces using React.js and Tailwind CSS, focusing on usability, performance, and cross-browser compatibility.",
     className: "md:col-span-2",
-    thumbnail: "/exp2.svg",
+    thumbnail: "/portfolio/exp2.svg",
   },
   {
     id: 3,
     title: "Digital Marketing Executive ",
     desc: "Developed and executed social media, SEO, and email marketing strategies for small businesses. Increased lead conversion through targeted ad campaigns and audience insights.",
     className: "md:col-span-2",
-    thumbnail: "/exp3.svg",
+    thumbnail: "/portfolio/exp3.svg",
   },
   {
     id: 4,
     title: "Shopify Store Manager – Dropshipping Projects",
     desc: "Launched and managed multiple Shopify stores, handling product research, store setup, order fulfillment, and customer support. Scaled revenue through Facebook and Google ads.",
     className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
+    thumbnail: "/portfolio/exp4.svg",
   },
 ];
 
 export const socialMedia = [
   {
     id: 1,
-    img: "/link.svg",
+    img: "/portfolio/link.svg",
     url: "www.linkedin.com/in/tareqee",
   },
   {
    id: 2,
-    img: "/git.svg",
+    img: "/portfolio/git.svg",
     url: "https://github.com/lebnolix",
   },
  {
     id: 3,
-    img: "/wat.svg",
+    img: "/portfolio/wat.svg",
     url: "https://api.whatsapp.com/send?phone=923133295908", // Use intl format, e.g. 919876543210
   },
 ];
